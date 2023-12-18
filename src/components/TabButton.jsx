0f6088dd3aebdc111ceb0e72779f8973e48f2d31
kkhowe123 <keanuhowe3@gmail.com>
2023-12-18 12:01:39 -0500

    Fixed the onclick for the TabButton component

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -2,7 +2,7 @@ export default function TabButton({children, onSelect}) //onSelect prop is use t
 { 
     return(
 
-        <li><button onSelec={handleSelect}>{children}</button></li> // using children prop to display the content between the TabButton component in th App components
+        <li><button onClick={onSelect}>{children}</button></li> // using children prop to display the content between the TabButton component in th App components
 
     );
 }
