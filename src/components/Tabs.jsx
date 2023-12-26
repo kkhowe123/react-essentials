8364340d54d3aac19e0cac26621983c92632c60e
kkhowe123 <keanuhowe3@gmail.com>
2023-12-26 17:01:29 -0500

    set the default prop value- final commit for this project

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,11 +1,13 @@
 // this is used as a wrapper componet for other components that needs to display a title and conent/description
-export default function Tabs({children,buttons}){
-    
+export default function Tabs({children,buttons, buttonsContainer = 'menu'}){
+    //"menu" is used to set the default value of the prop buttonContaciner
+    const ButtonsContainer = buttonsContainer; 
+    //above code is used to set the name of the ButtonContainer below to one of the build in html tags eg div,menu, ul,li//
     return(
     <>
-    <menu>
+    <ButtonsContainer>
         {buttons}
-    </menu>
+    </ButtonsContainer>
     {children}
     </>
 ); 
